784cd32111ffee0127a7336c9e69a8a06c811a91
Tarushi Arora <91682167+tarushiarora@users.noreply.github.com>
2025-03-31 02:17:59 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,13 +27,13 @@ app.use('/api/foods', foodRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-const publicFolder = path.join(__dirname, 'public');
-app.use(express.static(publicFolder));
+// const publicFolder = path.join(__dirname, 'public');
+// app.use(express.static(publicFolder));
 
-app.get('*', (req, res) => {
-  const indexFilePath = path.join(publicFolder, 'index.html');
-  res.sendFile(indexFilePath);
-});
+// app.get('*', (req, res) => {
+//   const indexFilePath = path.join(publicFolder, 'index.html');
+//   res.sendFile(indexFilePath);
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
