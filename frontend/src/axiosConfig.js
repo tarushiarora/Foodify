02f7aa9d69d539a242c774c37981ab29c7e6c740
Tarushi Arora <91682167+tarushiarora@users.noreply.github.com>
2025-03-31 01:40:52 +0530

    Update axiosConfig.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : '/';
+  process.env.REACT_APP_API_URL || 'http://localhost:5000';
