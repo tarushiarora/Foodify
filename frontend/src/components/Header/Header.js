5ccc4bb09b7ff928fd537d8be4a9fab2685999bf
Tarushi Arora <91682167+tarushiarora@users.noreply.github.com>
2025-03-31 00:46:16 +0530

    Update Header.js

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -13,7 +13,7 @@ export default function Header() {
     <header className={classes.header}>
       <div className={classes.container}>
         <Link to="/" className={classes.logo}>
-          Eat On Wheels
+          Foodify
         </Link>
         <nav>
           <ul>
